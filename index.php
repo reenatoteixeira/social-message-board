df26506fb2b6a6e1d56b1f2a831a44941e8cda93
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-05-01 20:22:01 -0300

    feat: update home layout

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once('config/database.php');
 ?>
 
@@ -12,7 +12,7 @@ require_once('config/database.php');
   <title>Social Messager</title>
 </head>
 
-<body class="bg-sky-200 px-4">
+<body class="bg-sky-200">
   <header>
     <a href="/">
       LOGO
@@ -23,8 +23,8 @@ require_once('config/database.php');
     </ul>
   </header>
 
-  <main class="">
-    <section class="grid grid-cols-4 gap-2">
+  <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <section class="grid grid-cols-1">
       <div class="bg-white p-4 shadow-lg">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laborum minus rerum laboriosam recusandae repudiandae excepturi quaerat, ducimus eos earum, porro culpa molestiae quae eligendi veniam iste voluptate aliquid eaque!</p>
       </div>
